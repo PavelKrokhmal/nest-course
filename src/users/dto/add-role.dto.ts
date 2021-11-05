d1f87f8165aa4d8cf6d7eab5cb4ca1a8912f1a5a
developer <pavel.krokhmal.dev@gmail.com>
2021-11-05 13:23:40 +0200

    implemented global validation pipe

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,9 +1,12 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsNumber, IsString} from "class-validator";
 
 export class AddRoleDto {
     @ApiProperty({example: 'admin', description: 'User role'})
+    @IsString({message: 'Must be a string'})
     readonly value: string;
 
     @ApiProperty({example: '1', description: 'User id'})
+    @IsNumber({}, {message: 'Must be a number'})
     readonly userId: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({example: 'user@mail.com', description: 'User email'})
+    @IsString({message: 'Must be a string'})
+    @IsEmail({}, {message: 'Incorrect email'})
     readonly email: string;
 
     @ApiProperty({example: '12345678', description: 'User password'})
+    @IsString({message: 'Must be a string'})
+    @Length(4, 16, {message: 'Length must be in range 4-16 symbols'})
     readonly password: string;
 }
